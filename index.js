48064fe495864377dac556cd79169c2ce5fac46e
Hans Tang <hanstang007@gmail.com>
2019-01-10 18:49:25 -0800

    commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) =>{
+    console.log('hello world');
     res.send('hello world');
 });
 
-app.listen(3000, ()=>{console.log("we are listening on port 3000!")});
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
